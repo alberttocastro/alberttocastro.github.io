725333569d041b9d28b580cdd22c3fc2e863b25f
alberttocastro <albertto.programmer@gmail.com>
2021-05-28 16:05:33 -0300

    Create mobile navbar

---
+++ .vuepress/theme/components/MobileNav.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-navbar toggleable variant="faded" class="d-md-none bg-primary" fixed="top">
+    <b-navbar-brand href="#">@alberttocastro</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item
+          v-for="link in this.$site.themeConfig.sidebar"
+          v-bind:key="link[0]"
+          :href="link[0]"
+          >{{ link[1] }}</b-nav-item
+        >
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
