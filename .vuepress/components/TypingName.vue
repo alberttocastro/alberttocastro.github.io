7061740d0c32e9923c4ebb3ae4a60a2b0b4bef0f
alberttocastro <albertto.programmer@gmail.com>
2021-05-29 13:36:26 -0300

    Typing name

---
+++ .vuepress/components/TypingName.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1 id="alberttocastro"></h1>
+</template>
+
+<script>
+import Typed from 'typed.js';
+export default {
+  mounted: function() {
+    let typed = new Typed('#alberttocastro', {
+      strings: ["@alberttocastro", "Alberto Castro"],
+      typeSpeed: 100,
+      showCursor: false,
+      smartBackspace: true,
+      backSpeed: 40,
+      backDelay: 3000,
+      loopCount: 3,
+      loop: true
+    });
+  }
+}
+</script>
+
+<style>
+
+h1 {
+  min-height: 3rem;
+  word-break: break-word;
+}
+
+</style>
