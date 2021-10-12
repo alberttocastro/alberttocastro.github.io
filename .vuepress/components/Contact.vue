9445cead5b43221627ac18cc4185b0cabc17832e
alberttocastro <albertto.programmer@gmail.com>
2021-10-12 13:55:22 -0300

    Contract

---
+++ .vuepress/components/Contact.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="d-flex align-content-center">
+      <div class="flex-grow-0">
+        <img :src="icon" />
+      </div>
+      <div class="flex-grow-1 d-flex align-items-center">
+        <div class="flex-grow-1 text-center m-0 px-4">
+          <slot />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: String,
+  },
+};
+</script>
+
+<style scoped>
+
+p {
+  margin: 0;
+  overflow-wrap: anywhere;
+}
+
+</style>
