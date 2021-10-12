934f383faa7339a2ca713fed47226d90133b0aad
alberttocastro <albertto.programmer@gmail.com>
2021-10-12 14:40:02 -0300

    Adds page title

---
+++ .vuepress/components/PageTitle.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="d-flex">
+    <div class="flex-grow-1 flex-shrink-1 d-flex align-items-center">
+      <div class="line"></div>
+    </div>
+    <div class="flex-grow-1 text-center title">
+      <h3>
+        <slot />
+      </h3>
+    </div>
+    <div class="flex-grow-1 flex-shrink-1 d-flex align-items-center">
+      <div class="line"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.line {
+  border: 1px solid #ffffff;
+  max-height: 0;
+  width: 100%;
+}
+
+h1, p {
+  margin: 0;
+}
+</style>
