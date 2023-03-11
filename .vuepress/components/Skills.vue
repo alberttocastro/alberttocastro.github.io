733069a7a9ef5ed84b2693efa998a9785f96d1b7
Alberttocastro <albertto.programmer@gmail.com>
2023-03-11 20:03:45 -0300

    Skill bar

--- .vuepress/components/Skills.vue
+++ .vuepress/components/Skills.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="d-flex my-3 pl-3">
+  <div class="d-flex my-3 pl-3 pb-2">
     <slot />
   </div>
 </template>
@@ -14,7 +14,28 @@ export default {
 
 div {
 
-  overflow: scroll;
+  overflow-x: scroll;
+  overflow-y: hidden;
+
+  // Simple
+  &::-webkit-scrollbar {
+    width: 6px;
+    height: 6px;
+  }
+  &::-webkit-scrollbar-track {
+    border-radius: 10px;
+    background: rgba(0,0,0,0.1);
+  }
+  &::-webkit-scrollbar-thumb{
+    border-radius: 10px;
+    background: rgba(0,0,0,0.2);
+  }
+  &::-webkit-scrollbar-thumb:hover{
+    background: rgba(0,0,0,0.4);
+  }
+  &::-webkit-scrollbar-thumb:active{
+    background: rgba(0,0,0,.9);
+  }
 
   img {
     margin: 0 1rem;
