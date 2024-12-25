5858c116433fd0f9044fc9f4518ba9543fb51463
AlberttoCastro <albertto.programmer@gmail.com>
2024-12-25 14:55:36 -0300

    Profile

--- .vuepress/theme/layouts/LinksLayout.vue
+++ .vuepress/theme/layouts/LinksLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="link-bg" fluid class="text-center">
     <img
-      src="../assets/aboutwritter.png"
+      src="../assets/profile.png"
       class="my-3 round"
       style="max-width: 40vw;"
     />
